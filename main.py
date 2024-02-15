8d80375facb61bf2c8cbbe545b9376d68df11a1d
IllyaMoore <148194279+IllyaMoore@users.noreply.github.com>
2024-02-15 20:55:00 +0200

    add

--- main.py
+++ main.py
@@ -1,64 +1,121 @@
-import os
-import telegram
-from telegram.ext import CommandHandler, MessageHandler, filters, ConversationHandler, Updater
-import openpyxl
+import datetime as dt
+import json
 
-# Змінна для зберігання даних стану розмови
-START, CHOOSE_FILE, READ_FILE = range(3)
+import requests
+from flask import Flask, jsonify, request
+from datetime import datetime
 
+API_TOKEN = ""
+RSA_API_KEY = ""
 
-def start(update, context):
-    update.message.reply_text("Привіт! Я Telegram бот для роботи з Excel таблицями. "
-                              "Відправте /open щоб відкрити файл Excel.")
-
-    return START
+app = Flask(__name__)
 
 
-def open_excel(update, context):
-    update.message.reply_text("Будь ласка, відправте Excel-файл, щоб його відкрити.")
+def generate_forecast(location: str, data: str, aqi: str):
+    url_base_url = "http://api.weatherapi.com"
+    url_api = "v1"
+    url_endpoint = ""
+    url_key = f"?key={RSA_API_KEY}"
+    url_location = ""
+    url_data = ""
+    url_aqi = ""
+    date_datatime = datetime.strptime(data, "%Y-%m-%d")  # перетворюємо строку в datatime
+    now = datetime.now()
 
-    return CHOOSE_FILE
-
-
-def read_excel(update, context):
-    file = context.user_data.get("file")
-    if file:
-        workbook = openpyxl.load_workbook(file)
-        sheet = workbook.active
-
-        # Отримуємо дані з таблиці і відправляємо їх користувачеві
-        data = "\n".join(["\t".join([str(cell.value) for cell in row]) for row in sheet.iter_rows()])
-        update.message.reply_text(f"Зміст Excel-файлу:\n{data}")
+    if date_datatime.date() == now.date():
+        url_endpoint = "current.json"
+    elif date_datatime.date() < now.date():
+        url_endpoint = "history.json"
     else:
-        update.message.reply_text("Файл Excel не був обраний. Відправте /open щоб відкрити файл знову.")
+        url_endpoint = "future.json"
 
-    return ConversationHandler.END
+    if location:
+        url_location = f"&q={location}"
 
+    if data:
+        url_data = f"&dt={data}"
 
-def main():
-    TOKEN = 'YOUR_BOT_TOKEN'
-    updater = Updater(token=TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    if aqi:
+        url_aqi = f"&aqi={aqi}"
 
-    # Додаємо обробники команд
-    start_handler = CommandHandler('start', start)
-    open_excel_handler = CommandHandler('open', open_excel)
-    read_excel_handler = MessageHandler(
-        filters.document.mime_type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"), read_excel)
+    url = f"{url_base_url}/{url_api}/{url_endpoint}{url_key}{url_location}{url_data}{url_aqi}"
 
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(open_excel_handler)
-    dispatcher.add_handler(ConversationHandler(
-        entry_points=[open_excel_handler],
-        states={
-            CHOOSE_FILE: [read_excel_handler]
-        },
-        fallbacks=[]
-    ))
+    payload = {}
+    headers = {}
 
-    updater.start_polling()
-    updater.idle()
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return json.loads(response.text)
 
 
-if __name__ == '__main__':
-    main()
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route("/")
+def home_page():
+    return "<p><h2>KMA L2: Python Saas.</h2></p>"
+
+
+@app.route(
+    "/content/api/v1/integration/generate",
+    methods=["POST"],
+)
+def weather_endpoint():
+    start_dt = dt.datetime.now()
+    json_data = request.get_json()
+
+    if json_data.get("token") is None:
+        raise InvalidUsage("token is required", status_code=400)
+
+    token = json_data.get("token")
+
+    if token != API_TOKEN:
+        raise InvalidUsage("wrong API token", status_code=403)
+
+    name = ""
+    if json_data.get("requester_name"):
+        name = json_data.get("requester_name")
+
+    location = ""
+    if json_data.get("location"):
+        location = json_data.get("location")
+
+    data = ""
+    if json_data.get("data"):
+        data = json_data.get("data")
+
+    aqi = ""
+    if json_data.get("aqi"):
+        aqi = json_data.get("aqi")
+
+    weather = generate_forecast(location, data, aqi)
+    end_dt = dt.datetime.now()
+
+    result = {
+        "event_start_datetime": start_dt.isoformat(),
+        "event_finished_datetime": end_dt.isoformat(),
+        "event_duration": str(end_dt - start_dt),
+        "requester_name": name,
+        "weather": weather,
+    }
+
+    return result
